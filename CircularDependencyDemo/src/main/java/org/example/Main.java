26b364e809bc4f72795ca0b9ed7937be39662d50
rakeshullengula56 <ramullengularakesh@gmail.com>
2026-06-22 21:49:33 +0530

    how to resolve CD in SpringBoot using @Autowired, but not recommended

--- CircularDependencyDemo/src/main/java/org/example/Main.java
+++ CircularDependencyDemo/src/main/java/org/example/Main.java
@@ -11,5 +11,7 @@ public class Main {
         orderService.placeOrder();
 
         /*Circular dependency is resolved using @Autowired but springBoot 2.5++ doesn't allow it*/
+        //in SB by default spring.main.allow-circular-reference=false;
+        // but do not use it, it is a bad coding style
     }
 }
